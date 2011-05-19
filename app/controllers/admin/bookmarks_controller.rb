e52fc0c63b28e032ff7d872c9733fbad28e27510
Edmund Haselwanter <edmund@haselwanter.com>
2011-05-19 14:04:21 +0200

    demo works

--- app/controllers/admin/bookmarks_controller.rb
+++ app/controllers/admin/bookmarks_controller.rb
@@ -1,8 +1,18 @@
-class Admin::BookmarksController < ApplicationController
+class Admin::BookmarksController < Admin::ResourceController
 
   include Globalize2::GlobalizedFieldsControllerExtension
 
-  def index
-    @bookmarks = Bookmark.find(:all,:include =>:translations)
-  end
+#  def index
+#    @bookmarks = Bookmark.find(:all,:include =>:translations)
+#  end
+
+#  def new
+#    @bookmark = Bookmark.new
+#  end
+#
+#  def edit
+#    @bookmark = Bookmark.find(params[:id])
+#  end
+
+
 end
